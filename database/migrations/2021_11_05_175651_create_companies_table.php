a88ae4ca4e680aaefd7ab058eb4109057dfa319f
MJ ALBDOUR <mjalbdour@gmail.com>
2021-11-05 20:41:22 +0200

    Created company model migration

---
+++ database/migrations/2021_11_05_175651_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('email')->unique()->index();
+            $table->string('address');
+            $table->string('phone_number');
+            $table->unsignedBigInteger('manager_id');
+            $table->timestamps();
+            $table->foreign('manager_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
