44f5c591ecf7d4993fbbfe3f5da58370badb58ac
MJ ALBDOUR <mjalbdour@gmail.com>
2021-11-05 22:03:04 +0200

    Created vehicle migration

---
+++ database/migrations/2021_11_05_184622_create_vehicles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('size', array_values(config('constants.vehicle_sizes')));
+            $table->unsignedFloat('price_in_amman');
+            $table->unsignedFloat('price_outside_amman');
+            $table->unsignedBigInteger('company_id');
+            $table->timestamps();
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
