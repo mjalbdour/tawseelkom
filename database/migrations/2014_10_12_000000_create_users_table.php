630b7a993ab7d77895bf88d2312521fa02fc908d
MJ ALBDOUR <mjalbdour@gmail.com>
2021-11-05 20:39:38 +0200

    Added phone_number, address attr.s to migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number')->unique();
+            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });
