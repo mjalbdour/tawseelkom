18cd85b14d82df4a8e38d828f9419c2ea5025a4c
MJ ALBDOUR <mjalbdour@gmail.com>
2021-11-05 20:41:05 +0200

    Created company factory

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->company(),
+            'email' => $this->faker->unique()->email(),
+            'phone_number' => $this->faker->unique()->phoneNumber(),
+            'address' => $this->faker->address(),
+        ];
+    }
+
+    public function manager()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'manager_id' => $attributes['id'],
+            ];
+        });
+    }
+}
