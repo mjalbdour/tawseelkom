6d8d2c06465f8e31b83b47295eef930aa09ce43a
Mohammad Al Bdour <mjalbdour@gmail.com>
2021-12-01 20:58:49 +0200

    Created company controller

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // user must be of role superadmin
+        $companies = Company::all();
+        return view("companies.index", compact('companies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // user must be of role superadmin
+        return view('compnies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // user must be of role superadmin
+        $validation = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'phone_number' => 'required',
+            'address' => 'required',
+            'manager_id' => 'required',
+        ]);
+        if ($validation->fails()) {
+            return redirect()->back()->with($validation->getMessageBag());
+        }
+
+        Company::create($request->all());
+
+        return redirect('companies.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Company $company
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Company $company)
+    {
+        return view('companies.show', compact('company'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Company $company
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Company $company)
+    {
+        return view('companies.edit', compact('company'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Company $company
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Company $company)
+    {
+        // user must be of role superadmin
+        $validation = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'phone_number' => 'required',
+            'address' => 'required',
+            'manager_id' => 'required',
+        ]);
+        if ($validation->fails()) {
+            return redirect()->back()->with($validation->getMessageBag());
+        }
+
+        $company->fill($request->all())->save();
+
+        return redirect('companies.show', compact('company'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Company $company
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Company $company)
+    {
+        $company->delete();
+        return redirect('companies.index');
+    }
+}
