a0f18243e421403046690594a565eb4b324ccfbb
Mohammad Al Bdour <mjalbdour@gmail.com>
2021-12-11 17:59:39 +0200

    Added role to fillables

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -22,7 +21,8 @@ class User extends Authenticatable
         'email',
         'password',
         'phone_number',
-        'address'
+        'address',
+        'role'
     ];
 
     /**
